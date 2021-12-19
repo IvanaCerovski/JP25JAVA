c66ee5ae97d636d8dad8c1e2c101be7f72f12ab4
IvanaCerovski <93525137+IvanaCerovski@users.noreply.github.com>
2021-12-19 11:34:40 +0100

    Update TypesAndVariables.java

--- MyFirstProject/src/TypesAndVariables.java
+++ MyFirstProject/src/TypesAndVariables.java
@@ -4,14 +4,11 @@ public class TypesAndVariables {
 		int myVariable; //deklariram varijablu naziva myVariable koja sadrži tip podatka int
 		myVariable = 5; //pokrećem myVariable s vrijednosti 5
 		
-		System.out.println(myVariable);
-		
-		myVariable = 10;
-		
-		System.out.println(myVariable);
-		
-		myVariable = myVariable + 1;
-		System.out.println(myVariable);
+		int myVar2 = 7;
+		System.out.println(myVariable + myVar2);
 	}
+	
 
 }
+
+
