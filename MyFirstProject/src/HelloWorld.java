63ac1d9eab3048b5f0bb01dd8f3f941e38ccb08e
IvanaCerovski <93525137+IvanaCerovski@users.noreply.github.com>
2021-12-19 11:02:37 +0100

    Vježba komentari

--- MyFirstProject/src/HelloWorld.java
+++ MyFirstProject/src/HelloWorld.java
@@ -1,6 +1,12 @@
 
 public class HelloWorld {
+	
+	//komentar
+	//main je point of entry za program
+	//shortcut main+space
 public static void main(String[] args) {
+	//ispisuje "Hello, World!" u konzolu
+	//sysout ctrl+space
 	System.out.println("Hello, World!");
 }
 }
