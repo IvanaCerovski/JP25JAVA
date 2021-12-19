da5a475324ae1011430c0905374bb051d0a37326
IvanaCerovski <93525137+IvanaCerovski@users.noreply.github.com>
2021-12-19 11:38:53 +0100

    Integer u double (doda .0)

--- MyFirstProject/src/TypesAndVariables.java
+++ MyFirstProject/src/TypesAndVariables.java
@@ -4,11 +4,16 @@ public class TypesAndVariables {
 		int myVariable; //deklariram varijablu naziva myVariable koja sadrži tip podatka int
 		myVariable = 5; //pokrećem myVariable s vrijednosti 5
 		
-		int myVar2 = 7;
+		int myVar2 = 7; //deklariram i pokrećem varijablu u jednoj liniji
 		System.out.println(myVariable + myVar2);
+		
+		double x = 2.5;
+		System.out.println(x);
+		
+		x = myVariable;
+		System.out.println(x);
 	}
 	
-
 }
 
 
