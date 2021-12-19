d11e138dd1e3a379d5356531a2afdfb4a767758d
IvanaCerovski <93525137+IvanaCerovski@users.noreply.github.com>
2021-12-19 12:02:00 +0100

    Metoda vraća vrijednost int

--- MyFirstProject/src/Methods.java
+++ MyFirstProject/src/Methods.java
@@ -4,10 +4,17 @@ public static void main(String[] args) {
 sayHelloTo("Ivana");
 sayHelloWorld();
 
+int x = returnFive();
+System.out.println(x);
+
 }
 
 static void sayHelloTo(String name) {
 	System.out.println("Hello, " + name);
+}
+
+static int returnFive() {
+	return 5;
 }
 
 static void sayHelloWorld() {
