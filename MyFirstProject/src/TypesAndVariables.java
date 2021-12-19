145956e4045050185aca256e2d59abe07a5dbd7e
IvanaCerovski <93525137+IvanaCerovski@users.noreply.github.com>
2021-12-19 11:21:13 +0100

    Deklariranje varijable

---
+++ MyFirstProject/src/TypesAndVariables.java
@@ -0,0 +1,10 @@
+
+public class TypesAndVariables {
+	public static void main(String[] args) {
+		int myVariable; //deklariram varijablu naziva myVariable koja sadrži tip podatka int
+		myVariable = 5; //pokrećem myVariable s vrijednosti 5
+		
+		System.out.println("myVariable");
+	}
+
+}
