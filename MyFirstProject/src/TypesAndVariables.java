8fd612e8f3963fcad3eb1a09291a72010a1204d4
IvanaCerovski <93525137+IvanaCerovski@users.noreply.github.com>
2021-12-19 11:25:57 +0100

    Dodana int varijabla

--- MyFirstProject/src/TypesAndVariables.java
+++ MyFirstProject/src/TypesAndVariables.java
@@ -4,7 +4,14 @@ public class TypesAndVariables {
 		int myVariable; //deklariram varijablu naziva myVariable koja sadrži tip podatka int
 		myVariable = 5; //pokrećem myVariable s vrijednosti 5
 		
-		System.out.println("myVariable");
+		System.out.println(myVariable);
+		
+		myVariable = 10;
+		
+		System.out.println(myVariable);
+		
+		myVariable = myVariable + 1;
+		System.out.println(myVariable);
 	}
 
 }
