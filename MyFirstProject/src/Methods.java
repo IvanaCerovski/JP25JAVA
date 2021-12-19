169ebf62364ee6a967970bd1b719ca0f878d2e2a
IvanaCerovski <93525137+IvanaCerovski@users.noreply.github.com>
2021-12-19 11:58:14 +0100

    Metode

---
+++ MyFirstProject/src/Methods.java
@@ -0,0 +1,16 @@
+
+public class Methods {
+public static void main(String[] args) {
+sayHelloTo("Ivana");
+sayHelloWorld();
+
+}
+
+static void sayHelloTo(String name) {
+	System.out.println("Hello, " + name);
+}
+
+static void sayHelloWorld() {
+	System.out.println("Hello, world!");
+}
+}
